df0fb6e9c1cbc597e8e176e3b902acf7424f57da
Evgeny Yakovlev <eyakovlev3@gmail.com>
2020-12-13 20:55:23 +0100

    Cleanup initrams script
    Signed-off-by: Evgeny Yakovlev <eyakovlev3@gmail.com>

--- initramfs.sh
+++ initramfs.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-set -e
-
 INITRAMFS_ROOT=$(realpath ${INITRAMFS_ROOT:-./initramfs})
+BUSYBOX_PATH=$(realpath ${BUSYBOX_PATH:-/bin/busybox})
 
 [ -d $INITRAMFS_ROOT ] && rm -rf $INITRAMFS_ROOT
 
 mkdir -p $INITRAMFS_ROOT/{bin,dev,etc,lib,proc,root,sbin,sys,/usr/bin,/usr/sbin,/usr/share} 
-pushd $INITRAMFS_ROOT
+pushd $INITRAMFS_ROOT > /dev/null
 
 ln -s lib lib64
 
 # Copy busybox
-cp /bin/busybox bin/
+[ -f "$BUSYBOX_PATH" ] || { echo Busybox is not installed; exit -1; }
+cp $BUSYBOX_PATH bin/
 chmod +x bin/busybox
 
 # Copy external non-static binaries
 DYNBINS="bash lspci lscpu lstopo numactl htop strace"
 for b in $DYNBINS; do
 	binpath=$(which $b)
+	[ -z "$binpath" ] && { echo $b is not installed; exit -1; }
+
 	cp $binpath bin/
 	for f in $(ldd $binpath | cut -d '(' -f 1 | cut -d '>' -f 2 | grep -v vdso); do
 		cp $f lib/
@@ -51,4 +53,4 @@ chmod +x init
 
 # Make the initramfs image
 find . | cpio -H newc -o > ../initramfs.cpio
-popd
+popd > /dev/null
