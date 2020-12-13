8331de0aefb84740f7975dd0bee3f80884dfd252
Evgeny Yakovlev <eyakovlev3@gmail.com>
2020-12-13 21:05:34 +0100

    Cleanup build script
    Signed-off-by: Evgeny Yakovlev <eyakovlev3@gmail.com>

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-set -x
+set -e
 
 CONFIG=${CONFIG:-config.debug}
 BINDIR=$(realpath ${BINDIR:-./build})
 
-mkdir $BINDIR > /dev/null
+[ ! -d $BINDIR ] && mkdir $BINDIR > /dev/null
 cp $CONFIG $BINDIR/.config
 touch $BINDIR/initramfs-empty.txt
 make -C linux/ O=$BINDIR ARCH=x86 oldconfig
-make -C linux/ O=$BINDIR ARCH=x86 -j 12 bzImage | tee ./build.log
+make -C linux/ O=$BINDIR ARCH=x86 -j $(nproc) bzImage | tee ./build.log
 
 objcopy --only-keep-debug build/vmlinux build/vmlinux.debug
-strip -s build/vmlinux
+strip --strip-debug  build/vmlinux
